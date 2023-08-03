5dc4a8f6aab43138a001ebee895dddcd6279a2d2
Martin H. Pefaur <mpefaur@gmail.com>
2023-08-02 21:11:27 -0300

    Update baño
    Ahora el nofy se baña

--- components/Tamagochi.js
+++ components/Tamagochi.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import tamagochiStyles from '../styles/Tamagochi.module.css';
+import React, { useState, useEffect } from "react";
+import tamagochiStyles from "../styles/Tamagochi.module.css";
 
-const Tamagochi = () => {
-  const [randomNumber, setRandomNumber] = useState(null); // Inicialmente, el valor es null
+const Tamagochi = ({ showBathGif }) => {
+  const [randomNumber, setRandomNumber] = useState(null);
 
   useEffect(() => {
-    // Genera un número aleatorio entre 0 y 119 solo cuando el componente se monta inicialmente
     if (randomNumber === null) {
       const random = Math.floor(Math.random() * 120);
       setRandomNumber(random);
     }
-  }, [randomNumber]); // Utiliza el randomNumber como dependencia para ejecutar el efecto solo cuando cambie su valor
+  }, [randomNumber]);
 
   return (
     <div className={tamagochiStyles.tamagochiContainer}>
       <div className={tamagochiStyles.frame}>
-        {/* Utiliza el número aleatorio en la URL de la imagen solo si ya se ha generado */}
         {randomNumber !== null && (
           <img
             src={`https://storage.googleapis.com/nof-gamma/T1/${randomNumber}.png`}
             alt="Tamagochi"
             className={tamagochiStyles.tamagochiImage}
           />
+        )}
+        {showBathGif && (
+          <div className={tamagochiStyles.bathGifContainer}>
+            <img
+              src="https://cdn.discordapp.com/attachments/907599032623431681/1136370825340129400/shower.gif"
+              alt="Bath Gif"
+              className={`${tamagochiStyles.bathGif}`}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
         )}
       </div>
     </div>
--- pages/nofy.js
+++ pages/nofy.js
@@ -1,10 +1,19 @@
-// pages/nofy.js
-import React from "react";
+import React, { useState } from "react";
 import Tamagochi from "../components/Tamagochi";
 import ButtonBar from "../components/ButtonBar";
 import Stats from "../components/Stats";
+import styles from "../styles/Tamagochi.module.css";
 
-const nofy = () => {
+const Nofy = () => {
+  const [showBathGif, setShowBathGif] = useState(false);
+
+  const handleBathClick = () => {
+    setShowBathGif(true);
+    setTimeout(() => {
+      setShowBathGif(false);
+    }, 8000);
+  };
+
   const buttons = [
     {
       label: "ALIMENTAR",
@@ -24,16 +33,15 @@ const nofy = () => {
     {
       label: "BAÑAR",
       color: "red",
-      onClick: () => console.log("Botón ENTRENAR presionado"),
+      onClick: handleBathClick,
     },
     {
       label: "DORMIR",
       color: "brown",
-      onClick: () => console.log("Botón ENTRENAR presionado"),
+      onClick: () => console.log("Botón DORMIR presionado"),
     },
   ];
 
-  // Puedes reemplazar estos valores con los datos reales de tu aplicación
   const weight = 75;
   const happiness = 80;
   const skillLevel = 60;
@@ -42,7 +50,6 @@ const nofy = () => {
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      {/* Agrega estilos inline para reducir el espaciado */}
       <div style={{ display: "flex", justifyContent: "space-around", width: "100%", maxWidth: "700px", margin: "10px 0" }}>
         <Stats label="Hambre" value={weight} />
         <Stats label="Felicidad" value={happiness} />
@@ -50,8 +57,7 @@ const nofy = () => {
         <Stats label="Higiene" value={higiene} />
         <Stats label="Sueño" value={sueño} />
       </div>
-      <Tamagochi />
-      {/* Coloca los botones debajo del marco */}
+      <Tamagochi showBathGif={showBathGif} />
       <div style={{ display: "flex", justifyContent: "center", margin: "10px 0" }}>
         <ButtonBar buttons={buttons} />
       </div>
@@ -59,4 +65,4 @@ const nofy = () => {
   );
 };
 
-export default nofy;
+export default Nofy;
