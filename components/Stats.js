696496db1091dcb465d60e12daf65cffc0c48edd
Martin H. Pefaur <mpefaur@gmail.com>
2023-08-08 10:46:48 -0300

    Update Stats
    Ahora los stats cambian de color.

--- components/Stats.js
+++ components/Stats.js
@@ -3,11 +3,22 @@ import React from "react";
 import StatsStyles from "../styles/Stats.module.css";
 
 const Stats = ({ label, value }) => {
+  let fillBackgroundColorClass;
+
+  if (value <= 60) {
+    fillBackgroundColorClass = StatsStyles.greenFillBackground;
+  } else if (value <= 85) {
+    fillBackgroundColorClass = StatsStyles.yellowFillBackground;
+  } else {
+    fillBackgroundColorClass = StatsStyles.redFillBackground;
+  }
+
   return (
     <div className={StatsStyles.statBar}>
       <span className={StatsStyles.statLabel}>{label}</span>
       <div className={StatsStyles.statBarFill}>
-        <div className={StatsStyles.statBarFillInner} style={{ width: `${value}%` }} />
+        <div className={`${StatsStyles.statBarFillInner} ${fillBackgroundColorClass}`} style={{ width: `${value}%` }}>
+        </div>
       </div>
     </div>
   );
