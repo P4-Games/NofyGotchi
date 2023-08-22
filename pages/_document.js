36ad39501043aec756cd02f909eb5e325147613f
Martin H. Pefaur <mpefaur@gmail.com>
2023-08-22 09:39:32 -0300

    Add Favicon / discord link
    Agrego favicon y botones de invite al discord.

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <title>TamaNofy</title>
+        <meta name="description" content="Ahora Nofy, es un tamagotchi" />
+      </Head>
       <body>
         <Main />
         <NextScript />
