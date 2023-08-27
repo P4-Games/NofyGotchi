40af811bde2ae7c28635f8e1bc305a202f97ce70
Martin H. Pefaur <mpefaur@gmail.com>
2023-08-26 23:49:14 -0300

    Update Dificulty
    Agrego 3 lvls de dificultad

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,41 @@
-// index.js
-import React from "react";
+import React, { useState } from "react"; // Importa useState
 import ButtonBar from "../components/ButtonBar";
 import tamagochiStyles from '../styles/Tamagochi.module.css'; // Importa los estilos
 import Huevo from "../components/Huevo"; // Importa el componente
-
 import { useRouter } from 'next/router';
+
 
 const HomePage = () => {
   const router = useRouter();
+  const [selectedDifficulty, setSelectedDifficulty] = useState("normal"); // Estado para la dificultad
 
-  const handleAbrirHuevoClick = () => {
-    console.log("Huevo abierto");
-    router.push('/nofy');
+  const handleDifficultyClick = (difficulty) => {
+    setSelectedDifficulty(difficulty); // Actualiza el estado con la dificultad seleccionada
+    router.push({
+      pathname: '/nofy',
+      query: { difficulty }, // Pasa la dificultad seleccionada como query parameter
+    });
   };
 
-  const buttons = [
+  // Agrega botones de dificultad
+  const difficultyButtons = [
     {
-      label: "ABRIR  HUEVO",
-      color: "purple",
-      onClick: handleAbrirHuevoClick,
+      label: "Easy",
+      color: "green",
+      isActive: selectedDifficulty === "easy",
+      onClick: () => handleDifficultyClick("easy"),
+    },
+    {
+      label: "Normal",
+      color: "blue",
+      isActive: selectedDifficulty === "normal",
+      onClick: () => handleDifficultyClick("normal"),
+    },
+    {
+      label: "Hard",
+      color: "red",
+      isActive: selectedDifficulty === "hard",
+      onClick: () => handleDifficultyClick("hard"),
     },
   ];
 
@@ -27,8 +44,9 @@ const HomePage = () => {
       <div className={tamagochiStyles.tamagochiContainer}>
         <Huevo/> {/* Usa el componente Huevo aquí */}
       </div>
+      {/* Agrega los botones de dificultad */}
       <div style={{ display: "flex", justifyContent: "center", margin: "10px 0" }}>
-        <ButtonBar buttons={buttons} />
+        <ButtonBar buttons={difficultyButtons} />
       </div>
     </div>
   );
